70ac5b66a3fe2f8670c22c708c747e787b636354
zhang.xiaobin <1009247049@qq.com>
2020-09-25 07:38:13 +0800

    MDD: 调整README位置

---
+++ code/src/test/java/Test.java
@@ -0,0 +1,15 @@
+/**
+ * @ClassName: Test <br>
+ * @Description: Test <br>
+ * @Date: 2020/9/24 上午6:47 <br>
+ * @Author: zxb <br>
+ * @Version: 1.0 <br>
+ */
+public class Test {
+
+    public static void main(String[] args) {
+        int a = 9 / 2;
+        System.out.println(a);
+    }
+
+}
